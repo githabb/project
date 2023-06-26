59018af4fdfaaaa047bf7203d56994db2c5e1b6b
Ivan <yurievch@gmail.com>
2023-06-26 10:58:30 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// "use strict";
+// let number = 4.6;
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.age);
+
+// const result = confirm("Are you here&");
+
+// const answer = prompt("Are you 18", "18");
+// console.log(typeof(answer));
+
+// const category = 'toys'; 
+// console.log(`scdfbfgnhgng${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Hello, ${user}`);
+
+// console.log('arr' + '- ojgfgjikn');
+// console.log(5 + + '6');
+
+// let incr = 10,
+// decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+
+// let incr = 10,
+// decr = 10;
+
+// console.log(++incr);
+// console.log(--decr);
+
+
+// console.log(5%2);
+// console.log(2*5 == 8);
+
+const a = true;
+b = false;
+console.log(a && b);
